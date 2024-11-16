ca4b5a6c71b87880444d362e456f386bf8c8e974
Ekaterina Logutova <logutova.ekaterina@gmail.com>
2024-11-16 23:04:25 +0300

    generate_level work!

--- src/Generate_level.cpp
+++ src/Generate_level.cpp
@@ -5,8 +5,8 @@ Generate_level_window::Generate_level_window(Graph_lib::Point xy, int w, int h, 
 : Window_with_back{xy, w, h, title}, p{xy}, w{w}, h{h}, title{title},
 size_box{Graph_lib::Point{260, 250}, 200, 50, "size"}
 {
+    attach(size_box);
     size_box.add("10 x 10");
     size_box.add("15 x 15");
     size_box.add("20 x 20");
-    attach(size_box);
 }
