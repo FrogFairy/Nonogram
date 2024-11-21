91bbc05b5ba04b3a7d27d6057142de964fbecac6
Ekaterina Logutova <logutova.ekaterina@gmail.com>
2024-11-21 14:47:03 +0300

    try to fix save_button

--- src/Generate_level.cpp
+++ src/Generate_level.cpp
@@ -1,6 +1,19 @@
 #include "Generate_level.h"
 #include "Main_window.h"
 
+void Generate_level_window::cb_save_button(Graph_lib::Address, Graph_lib::Address addr)
+{
+    auto* pb = static_cast<Graph_lib::Button*>(addr);
+    static_cast<Generate_level_window&>(pb->window()).save_button();
+}
+void Generate_level_window::save_button()
+{
+    Generate_level_window win {p, w, h, title};
+    this->hide();
+    win.wait_for_button();
+    this->show();
+}
+
 Generate_level_window::Generate_level_window(Graph_lib::Point xy, int w, int h, const std::string &title)
 : Window_with_back{xy, w, h, title}, p{xy}, w{w}, h{h}, title{title},
 size_box{Graph_lib::Point{260, 250}, 200, 50, "size"},
@@ -11,6 +24,9 @@ image_chooser{Graph_lib::Point{260, 350}, 200, 50, " ", "choose image", "Image F
     size_box.add("10 x 10");
     size_box.add("15 x 15");
     size_box.add("20 x 20");
+
+    Graph_lib::Button save_button {Graph_lib::Point{590, 670}, 100, 20, "save", cb_save_button};
+    attach(save_button);
 }
 
 void Generate_level_window::cb_choose_file(Graph_lib::Address, Graph_lib::Address addr)
