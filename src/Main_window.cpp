753f521d21a72ec3e0ca367583d562d95ca49c41
Ekaterina Logutova <logutova.ekaterina@gmail.com>
2024-11-24 21:31:58 +0300

    add choose level window

--- src/Main_window.cpp
+++ src/Main_window.cpp
@@ -1,6 +1,6 @@
 #include "Main_window.h"
 #include "Generate_level.h"
-
+#include "Choose_level.h"
 
 void Main_window::cb_go_to_level(Graph_lib::Address, Graph_lib::Address addr)
 {
@@ -16,7 +16,7 @@ void Main_window::cb_create_level(Graph_lib::Address, Graph_lib::Address addr)
 
 void Main_window::go_to_level(std::string btn_label)
 {
-    Window_with_back win {p, w, h, title}; // создавать окно с уровнями; передавать в него label кнопки, на котором указаны размеры поля
+    Choose_level_window win {p, w, h, title, btn_label}; // создавать окно с уровнями; передавать в него label кнопки, на котором указаны размеры поля
     this->hide();
     win.wait_for_button();
     this->show();
