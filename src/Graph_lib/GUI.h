97ee9b998611f7eadd95695dc489089d6850594f
Daria Novoselova <novdasha535@yandex.ru>
2024-11-24 17:48:29 +0300

    add include in fltk.h

--- src/Graph_lib/GUI.h
+++ src/Graph_lib/GUI.h
@@ -1,4 +1,3 @@
-//
 // This is a GUI support code to the chapters 12-16 of the book
 // "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
 //
--- src/Graph_lib/fltk.h
+++ src/Graph_lib/fltk.h
@@ -6,6 +6,8 @@
 #include <FL/Enumerations.H>
 #include <FL/Fl.H>
 #include <FL/Fl_Button.H>
+#include <FL/Fl_Choice.H>
+#include <FL/Fl_File_Chooser.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Output.H>
 #include <FL/Fl_Window.H>
@@ -14,5 +16,7 @@
 #include <FL/Fl_GIF_Image.H>
 #include <FL/Fl_JPEG_Image.H>
 #include <FL/Fl_PNG_Image.H>
+#include <FL/Fl_Shared_Image.H>
+#include <FL/Fl_Image.H>
 
 #endif  // #ifndef FLTK_GUARD
