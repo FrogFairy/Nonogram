927b55e34daf162cb590b014519b4c3d6a3f98bd
Daria Novoselova <novdasha535@yandex.ru>
2024-12-06 01:28:50 +0300

    delete helping_func and move this func in database

--- src/helping_func.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-std::vector<int> size_to_int(const std::string& size)
-{
-    int w = std::stoi(size.substr(0, size.find("x")));
-    int h = std::stoi(size.substr(size.find("x" + 1)));
-    return std::vector<int> {w, h};
-}
