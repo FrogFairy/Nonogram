6cb3f860cd997e44055673164ef2a0155f79c9b6
Alice <spektoram@gmail.com>
2024-12-12 22:49:49 +0300

    added Position

---
+++ src/logic_board.cpp
@@ -0,0 +1,171 @@
+#include "logic_board.h"
+
+#include <random>
+#include <algorithm>
+
+void Logic_board::fill_row_digits() 
+{
+    for (int i = 0; i < correct.size(); ++i)
+    {
+        std::vector<std::vector<int>> intervals {};
+        int start = -1;
+        for (int j = 0; j < correct[0].size(); ++j)
+        {
+            if (correct[i][j] == 1)
+            {
+                if (start < 0)
+                    start = j;
+                ++finish_count;
+            }
+            if (start >= 0 && (correct[i][j] == 0 || j == correct[0].size() - 1))
+            {
+                if (correct[i][j] == 0)
+                    intervals.push_back(Position {start, j - 1});
+                else 
+                    intervals.push_back(Position {start, j});
+                start = -1;
+            }
+            if (current[i][j] == 1) ++correct_count;
+        }
+        if (!intervals.size())
+        {
+            intervals.push_back(Position{});
+        }
+        row_intervals.push_back(intervals);
+        max_rows = std::max(max_rows, (int) intervals.size());
+    }
+}
+
+void Logic_board::fill_col_digits()
+{
+    for (int i = 0; i < correct[0].size(); ++i)
+    {
+        std::vector<std::vector<int>> intervals {};
+        int end = -1;
+        for (int j = correct.size() - 1; j >= 0; --j)
+        {
+            if (correct[j][i] == 1)
+            {
+                if (end < 0)
+                    end = j;
+            }
+            if (end >= 0 && (correct[j][i] == 0 || j == 0))
+            {
+                if (correct[j][i] == 0)
+                    intervals.push_back(Position {j + 1, end});
+                else 
+                    intervals.push_back(Position {j, end});
+                end = -1;
+            }
+        }
+        if (!intervals.size())
+        {
+            intervals.push_back(Position {});
+        }
+        col_intervals.push_back(intervals);
+        max_cols = std::max(max_cols, (int) intervals.size());
+    }  
+}
+
+std::vector<int> Logic_board::row_changed(Position pos)
+{
+    Position res {};
+
+    int left = pos.y, right = pos.y;
+    while (true)
+    {
+        if ((left == 0 || left > 0 && (current[pos.x][left - 1] < 0 || current[pos.x][left - 1] % 2 == 0)) &&
+            (right == width - 1 || right < width - 1 && (current[pos.x][right + 1] < 0 || current[pos.x][right + 1] % 2 == 0)))
+            break;
+        if (left > 0 && current[pos.x][left - 1] > 0 && current[pos.x][left - 1] % 2 != 0)
+            --left;
+        if (right < width - 1 && current[pos.x][right + 1] > 0 && current[pos.x][right + 1] % 2 != 0)
+            ++right;
+    }
+
+    if ((left == 0 || left > 0 && correct[pos.x][left - 1] % 2 == 0) && 
+        (right == width - 1 || right < width - 1 && correct[pos.x][right + 1] % 2 == 0))
+    {
+        int col = std::find(row_intervals[pos.x].begin(), row_intervals[pos.x].end(), Position {left, right}) - row_intervals[pos.x].begin();
+        res {x, col};
+    }
+    return res;
+}
+
+std::vector<int> Logic_board::col_changed(Position pos)
+{
+    Position res {};
+
+    int down = pos.x, up = pos.x;
+    while (true)
+    {
+        if ((up == 0 || up > 0 && (current[up - 1][pos.y] < 0 || current[up - 1][pos.y] % 2 == 0)) &&
+            (down == height - 1 || down < height - 1 && (current[down + 1][pos.y] < 0 || current[down + 1][pos.y] % 2 == 0)))
+            break;
+        if (up > 0 && current[up - 1][pos.y] > 0 && current[up - 1][pos.y] % 2 != 0)
+            --up;
+        if (down < height - 1 && current[down + 1][pos.y] > 0 && current[down + 1][pos.y] % 2 != 0)
+            ++down;
+    }
+
+    if ((up == 0 || up > 0 && correct[up - 1][pos.y] % 2 == 0) && 
+        (down == height - 1 || down < height - 1 && correct[down + 1][pos.y] % 2 == 0))
+    {
+        int row = std::find(col_intervals[y].begin(), col_intervals[y].end(), Position {up, down}) - col_intervals[y].begin();
+        res {y, row};
+    }
+
+    return res;
+}
+
+std::vector<std::vector<int>> Logic_board::changed_digits(Position pos)
+{
+    std::vector<Position> res (2, Position {});
+    if (current[pos.x][pos.y] < 0 || current[pos.x][pos.y] % 2 == 0) return res;
+
+    res[0] = row_changed(Position{x, y});
+    res[1] = col_changed(Position{x, y});
+
+    return res;
+}
+
+void Logic_board::set_click(Position pos, int val)
+{
+    if (current[pos.x][pos.y] == -1)
+    {
+        current[pos.x][pos.y] = val;
+        empty.erase(std::find(empty.begin(), 
+                    empty.end(), std::vector<int> {pos.x, pos.y}));
+        if (correct[pos.x][pos.y] == 1) ++correct_count;
+    }
+    status = (correct_count == finish_count ? FINISH : (current[pos.x][pos.y] == correct[pos.x][pos.y] ? OK : MISTAKE));
+    if (status == MISTAKE)
+        current[pos.x][pos.y] = correct[pos.x][pos.y] + 4;
+}
+
+Position Logic_board::hint_click()
+{
+    if (correct_count == current.size() * current[0].size()) 
+        return std::vector<int> {};
+    
+    int rand_ind = std::rand() % empty.size();
+    Position pos {empty[rand_ind]};
+    current[pos.x]pos.[y] = correct[x][y] + 2;
+
+    empty.erase(empty.begin() + rand_ind);
+    if (correct[pos.x][pos.y] == 1)
+        ++correct_count;
+    status = (correct_count == finish_count ? FINISH : OK);
+
+    return pos;
+}
+
+void Logic_board::after_hint(Position pos)
+{
+    current[pos.x][pos.y] -= 2;
+}
+
+void Logic_board::after_mistake(Position pos)
+{
+    current[pos.x][pos.y] -= 4;
+}
---
+++ src/logic_board.h
@@ -0,0 +1,66 @@
+#ifndef LOGIC_BOARD_H
+#define LOGIC_BOARD_H
+
+#include "database.h"
+
+#include <vector>
+
+struct Position
+{
+    Position() : _x(0), _y(0) {};
+    Position(unsigned int x, unsigned int y) : _x{x}, _y{y} {}
+    unsigned int x() { return _x; }
+    unsigned int y() { return _y; }
+
+private:
+    unsigned int _x;
+    unsigned int _y;
+};
+
+struct Logic_board
+{
+public:
+    enum Response {OK, MISTAKE, FINISH};
+
+    Logic_board(Level& level) 
+        : current{level.current_values}, correct{level.correct_values},
+        empty{level.empty}, row_intervals{}, col_intervals{}, status{OK}
+    {
+        height = level.size.height();
+        width = level.size.width();
+        fill_col_digits();
+        fill_row_digits();
+        if (correct_count == finish_count) status = FINISH;
+    }
+     
+    void set_click(Position pos, int val);
+    Position hint_click();
+    void after_hint(Position pos);
+    void after_mistake(Position pos);
+
+    std::vector<int> row_changed(Position pos);
+    std::vector<int> col_changed(Position pos);
+    std::vector<std::vector<int>> changed_digits(Position pos);
+
+    void fill_row_digits();
+    void fill_col_digits();
+
+    int max_rows  = 0; // maximum of digits in rows
+    int max_cols = 0; // maximum of digits in cols
+
+    int width, height;
+    
+    std::vector<std::vector<std::vector<int>>> row_intervals;
+    std::vector<std::vector<std::vector<int>>> col_intervals;
+    std::vector<std::vector<int>> current;
+    std::vector<std::vector<int>> correct;
+    std::vector<std::vector<int>> empty;
+
+    Response status;
+
+private:
+    int correct_count = 0; // count of correct click (filled)
+    int finish_count = 0; // count of filled cells in correct
+};
+
+#endif // LOGIC_BOARD_H
